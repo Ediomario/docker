a6b9010d6ded0012a0346474ccf7b8754c918ccc
root <root@Linktel>
2023-09-08 08:26:03 -0300

    alterado documentos na pasta compose

--- portainer.sh
+++ portainer.sh
@@ -1,9 +1,17 @@
-Install Portainer --- Gerenciador de container
+#!/bin/bash
+
+#Install Portainer --- Gerenciador de container
 
 cd 
 docker volume create portainer_data
 
-docker run -d -p 8000:8000 -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+docker run -d \
+	-p 8000:8000 \
+	-p 7900:9000 \
+	--name portainer \
+	--restart=always \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v portainer_data:/data portainer/portainer-ce
 
 
 #Templates:
