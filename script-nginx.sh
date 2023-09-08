282fd2f474c91bf6c3af66cb8316182e19447965
Ediomario <root@Linktel>
2023-09-08 08:32:27 -0300

    alterado script nginx

--- script-nginx.sh
+++ script-nginx.sh
@@ -10,4 +10,12 @@
 mkdir -p ./docker-nginx
 
 #Docker installl conteiner
-docker run -d --memory-swap 2G  -m 1G --cpus 1 --name nginx --restart=always -p 80:80 -v ~/docker-nginx/html:/usr/share/nginx/html nginx
+
+docker run -d \
+	--memory-swap 2G \
+	-m 1G \
+	--cpus 1 \
+	--name nginx \
+	--restart=always \
+	-p 80:80 \
+	-v ~/docker-nginx/html:/usr/share/nginx/html nginx
